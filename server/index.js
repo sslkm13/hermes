fc4c8369b78ac4d39481b2d438bf62cb8763e5e3
zusr <sslkm13@gmail.com>
2018-11-23 08:39:43 +0530

    Authentication: GoogleStrategy-OAuth-Passport

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
+require('./services/passport');
+
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send({ hello: 'hello world!' });
-});
+require('./routes/authRoutes')(app);
 
-app.listen(5000);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
