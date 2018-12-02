a3dcfb12766b93e402a4972fb77f128888dc5761
zusr <sslkm13@gmail.com>
2018-12-02 19:06:18 +0530

    Front end changes for production

--- server/index.js
+++ server/index.js
@@ -24,5 +24,16 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+	// Express serves production assets(main.js, main.css)
+	app.use(express.static('client/build'));
+
+	// Express serves index.html if route is not recognized
+	const path = require('path');
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
